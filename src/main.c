c568373f8698ef98b4fd4fd10d726ad606d636b2
gondvalle <146117532+gondvalle@users.noreply.github.com>
2024-04-17 18:18:56 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -16,14 +16,14 @@ int	main(int argn, char **argv)
 {
 	t_fractal	fractal;
 
-	if (argn == 4 && ft_strncmp(argv[1], "julia", 5))
+	if (argn == 4 && ft_strncmp(argv[1], "julia", 5) == 0)
 	{
 		fractal.name = argv[1];
 	}
-	else if (argn == 2 && ft_strncmp(argv[1], "mandelbrot", 10))
+	else if (argn == 2 && ft_strncmp(argv[1], "mandelbrot", 10) == 0)
 		fractal.name = argv[1];
 	else
-		return (ft_putstr_fd("Error\n", 2), 0);
+		return (ft_putstr_fd("Pau\n", 2), 0);
 	plot_start(&fractal);
 	plot_render(&fractal);
 	mlx_loop(fractal.mlx_conect);
